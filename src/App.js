18400d2d994023583976be8670ba4de037543ada
hamoudafeki <hamouda125.fh@gmail.com>
2023-04-09 12:32:51 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./comp/NavBar";
+import Forme from "./comp/Forme";
+import Cards from "./comp/Cards";
+import Footer from "./comp/Footer";
+
+function App() {
+  return (
+    <div className="App">
+<NavBar />
+    <br/>
+    <br/>
+    <Cards/>  
+<br/>
+<br/>
+<Forme/>
+<br/>
+<br/>
+
+<footer>
+  <Footer/>
+</footer>
+    </div>
+  );
+}
+
+export default App;
